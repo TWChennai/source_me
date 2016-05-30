8affac594c957e3daac0d3624ee4d4dd5d73fe08
Raghavan <oneraghavan@gmail.com>
2016-05-30 22:34:11 +0530

    Raghavan : Use async library to simplify the code

--- github_api/followers.js
+++ github_api/followers.js
@@ -1,34 +1,58 @@
 var GitHubApi = require("github");
-var lodash = require('lodash')
+var _ = require('lodash');
+var async = require('async');
 
-  console.log("Hello world");
+var github = new GitHubApi({
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    timeout: 5000,
+    version: '3.0.0',
+    headers: {
+        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+    }
+});
 
-  var github = new GitHubApi({
-      // optional
-      debug: true,
-      protocol: "https",
-      host: "api.github.com", // should be api.github.com for GitHub
-      timeout: 5000,
-      version: '3.0.0',
-      headers: {
-          "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-      }
-  });
-  console.log(github.repos);
-  github.search.users({
-      q:'location:chennai+repos:>50',
-      sort:'repositories'
-  }, function(err, res) {
-    var items = res.items;
-    console.log("count - "+ res.total_count);
+var getUserRepos = function (users) {
+    var listOfCallsForRepos = _.map(users, function (user) {
+        return (function () {
+            console.info(user);
+            github.search.repos({
+                q: 'user:' + user
+            }, function (err, res) {
+                console.info("===========================");
+                console.log(JSON.stringify(res));
+            });
+        });
+    });
 
-    items.forEach(function(value){
-      console.log(JSON.stringify(value.login));
-      /*github.search.repos({
-        q:'user:' + value.login,
-        function(err, res){
-          console.log(JSON.stringify(res));
-        }
-      })*/
+    async.parallel(listOfCallsForRepos, function (err, result) {
+        console.info(err);
+        console.info(result)
     })
-  });
+
+};
+
+var processDeveloperRepos = function (res, next) {
+    var items = res.items;
+    next(null, _.map(items, "login"));
+};
+
+var getTopDevelopersInChennai = function (next) {
+    github.search.users({
+            q: 'location:chennai+repos:>50',
+            sort: 'repositories'
+        },
+        function (err, result) {
+            console.info(result);
+            next(err, result);
+        })
+};
+
+async.waterfall([
+    getTopDevelopersInChennai,
+    processDeveloperRepos,
+    getUserRepos
+], function (err, res) {
+    console.info(err);
+    console.info(res)
+});
