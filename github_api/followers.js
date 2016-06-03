4baa4533e6a493cccdb20d7f2ccfae3ff73c4e35
Raghavan <oneraghavan@gmail.com>
2016-06-03 20:11:49 +0530

    Raghavan : Add ability to export the result to excel sheet

--- github_api/followers.js
+++ github_api/followers.js
@@ -1,11 +1,12 @@
-var GitHubApi = require("github");
-var _ = require('lodash');
-var async = require('async');
-var profile = require('./profile');
-var Table = require('cli-table');
+const GitHubApi = require("github");
+const _ = require('lodash');
+const async = require('async');
+const Table = require('cli-table');
+const json2xls = require('json2xls');
+const fs = require('fs');
 
-var github = new GitHubApi({
-    //debug: true,
+const github = new GitHubApi({
+    debug: true,
     protocol: "https",
     host: "api.github.com",
     timeout: 5000,
@@ -24,12 +25,12 @@ github.authenticate({
 
 var getTopDevelopersInChennai = function (next) {
 
-    var developersInLocation = _.times(1, function (page) {
+    var developersInLocation = _.times(10, function (page) {
         return (function (callback) {
                 github.search.users({
-                        q: 'location:chennai+repos:>5',
+                        q: 'location:chennai+repos:>10',
                         sort: 'repositories',
-                        per_page: 10,
+                        per_page: 100,
                         page: page + 1
                     },
                     function (err, result) {
@@ -37,12 +38,14 @@ var getTopDevelopersInChennai = function (next) {
                             console.info(err);
                         }
                         callback(null, result);
+                        //sleep(500);
                     })
             }
         )
     });
 
     async.parallel(developersInLocation, function (err, response) {
+        console.info(response[0]);
         var items = _.filter(_.flatten(_.map(response, "items")), function (item) {
             return !_.isUndefined(item);
         });
@@ -58,7 +61,7 @@ var getTopDevelopersInChennai = function (next) {
 
 var enrichPersonalInfo = function (profiles, next) {
 
-    var developersInLocation = _.map(profiles, function (profile) {
+    var developerPersonalInfo = _.map(profiles, function (profile) {
         return (function (callback) {
 
                 github.user.getFrom(
@@ -75,61 +78,111 @@ var enrichPersonalInfo = function (profiles, next) {
                         profile.company = result.company ? result.company : "";
                         profile.blog = result.blog ? result.blog : "";
                         profile.hireable = result.hireable ? "Yes" : "No";
+                        sleep(500);
                         callback(null, profile);
                     })
             }
         )
     });
 
-    async.parallel(developersInLocation, function (err, response) {
-
+    async.series(developerPersonalInfo, function (err, response) {
         next(null, profiles)
     });
 
 
 };
 
-var EnrichRepos = function (profiles, next) {
+var EnrichReposInfo = function (profiles, next) {
     var listOfCallsForRepos = _.map(profiles, function (profile) {
         return (function (callback) {
             github.repos.getFromUser({
                 user: profile.loginId
             }, function (err, repositories) {
-                var ownedRepositories = _.filter(repositories, {fork:false});
-                var forkedRepositories = _.filter(repositories, {fork:true});
+                var ownedRepositories = _.filter(repositories, {fork: false});
+                var forkedRepositories = _.filter(repositories, {fork: true});
                 //TODO get languages from language url.
                 profile.languages = _.uniq(_.map(ownedRepositories, "language"));
                 profile.own_repository_count = ownedRepositories.length;
                 profile.forked_repository_count = forkedRepositories.length;
+                sleep(500);
                 callback(null, profile)
             });
         });
     });
 
-    async.parallel(listOfCallsForRepos, function (err, profiles) {
+    async.series(listOfCallsForRepos, function (err, profiles) {
         next(null, _.sortBy(_.reject(profiles, {own_repository_count: 0}), 'own_repository_count').reverse())
     })
 };
 
+var EnrichDeveloperActivity = function (profiles, next) {
+
+    var developersActivityInfo = _.map(profiles, function (profile) {
+        return (function (callback) {
+            github.events.getFromUser({
+                user: profile.loginId,
+                per_page: 100,
+                page: 1
+            }, function (err, result) {
+                if(err){
+                    console.info(err);
+                }
+                if(result){
+                    profile.releases_made = _.filter(result,{type: 'releaseEvent'}).length
+                    profile.push_count = _.filter(result,{type: 'PushEvent'}).length
+                    profile.pull_request_made = _.filter(result,{type: 'PullRequestEvent'}).length
+                    profile.pull_request_reviewed = _.filter(result,{type: 'PullRequestReviewCommentEvent'}).length
+                    profile.open_sourcing_private_repo_count = _.filter(result,{type: 'PublicEvent'}).length
+                    profile.wikis_contributed = _.filter(result,{type: 'GollumEvent'}).length
+                }
+                sleep(500);
+                callback(null,profile)
+            });
+        });
+    });
+
+    async.series(developersActivityInfo, function (err, profiles) {
+        next(null, profiles)
+    })
+};
+
+const sleep = function (milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+};
 
 var displayTopDeveloperProfiles = function (profiles) {
+
     var table = new Table({
         head: _.keys(profiles[0]),
-        colWidths: [20, 50, 50, 10, 10, 10, 10, 10 , 10]
+        colWidths: [20, 20, 10, 15, 20, 10, 20, 20, 20, 20, 20, 20, 20, 20, 20]
     });
 
     _.forEach(profiles, function (profile) {
         table.push(_.values(profile));
     });
-
     console.log(table.toString());
+};
+
+
+var exportProfilesToExcel = function(profiles){
+
+    var xls = json2xls(profiles);
+
+    fs.writeFileSync('data.xlsx', xls, 'binary');
 };
 
 async.waterfall([
     getTopDevelopersInChennai,
     enrichPersonalInfo,
-    EnrichRepos,
-    displayTopDeveloperProfiles
+    EnrichReposInfo,
+    EnrichDeveloperActivity,
+    //displayTopDeveloperProfiles,
+    exportProfilesToExcel
 ], function (err, res) {
     console.info(res)
 });
