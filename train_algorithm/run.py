1051b22c9955c77988b658de908bfa471388da32
D.Sailesh Dev <dsailesh@thoughtworks.com>
2016-06-22 13:07:26 +0530

    This patch adds the function to clean the raw data, converts data to numericals

--- train_algorithm/run.py
+++ train_algorithm/run.py
@@ -5,7 +5,19 @@ import matplotlib.pyplot as plt
 def get_data_from_csv():
     input_file = "./input/PastHires.csv"
     df = pd.read_csv(input_file, header = 0)
+    return df
 
 
-get_data_from_csv()
+def clean_up_data(df):
+    d = {'Y': 1, 'N': 0}
+    df['Hired'] = df['Hired'].map(d)
+    df['Employed?'] = df['Employed?'].map(d)
+    df['Top-tier school'] = df['Top-tier school'].map(d)
+    df['Interned'] = df['Interned'].map(d)
+    d = {'BS': 0, 'MS': 1, 'PhD': 2}
+    df['Level of Education'] = df['Level of Education'].map(d)    
+    return df
 
+
+data_frame = get_data_from_csv()
+clean_data_frame = clean_up_data(data_frame)
