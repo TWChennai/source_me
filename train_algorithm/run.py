ef7de5f720dfb4331d42f222c3cc8278ab878768
D.Sailesh Dev <dsailesh@thoughtworks.com>
2016-06-22 13:11:19 +0530

    Prints the class distribution for stats

--- train_algorithm/run.py
+++ train_algorithm/run.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,6 +20,15 @@ def clean_up_data(df):
     df['Level of Education'] = df['Level of Education'].map(d)    
     return df
 
+def print_class_distribution(df):
+    num_obs = len(df)
+    num_true = len(df.loc[df['Hired'] == 1])
+    num_false = len(df.loc[df['Hired'] == 0])
+    print("Number of True cases:  {0} ({1}%)".format(num_true, (num_true/num_obs) * 100))
+    print("Number of False cases: {0} ({1}%)".format(num_false, (num_false/num_obs) * 100))
+
 
 data_frame = get_data_from_csv()
 clean_data_frame = clean_up_data(data_frame)
+
+print_class_distribution(clean_data_frame)
