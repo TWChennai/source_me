212d098c5f68767269f207be3e3f1a6cd8cd8f27
D.Sailesh Dev <dsailesh@thoughtworks.com>
2016-06-26 14:55:21 +0530

    Adds a simple coomand line UI to input the predictor variables

--- predict_candidate/run.py
+++ predict_candidate/run.py
@@ -11,4 +11,38 @@ def importAlgorithms():
     lr_cv_model = joblib.load("algorithms/logistic_regression_cv")
     dt_model = joblib.load("algorithms/decision_tree")
 
+
+def getInput():
+    print("Enter the details of the candidate:\n")
+
+    print("Enter the experience in years rounded off to an integer:")
+    exp = raw_input()
+    print("") 
+    
+    print("Has the candidate studied in a top-tier school?(y/n):")
+    tt_school = raw_input()
+    tt_school = tt_school.lower()
+    print("")
+    
+    print("Has the candidate worked on a pet project?(y/n):")
+    pet_project = raw_input()
+    pet_project = pet_project.lower()
+    print("")
+
+    print("Any recognitions?(y/n):")
+    recog = raw_input()
+    recog = recog.lower()
+    print("")
+    
+    print("Any certifications?(y/n):")
+    cert = raw_input()
+    cert = cert.lower()
+    print("")
+
+    return exp, tt_school, pet_project, recog, cert
+
+
 importAlgorithms()
+exp, tt_school, pet_project, recog, cert = getInput()
+
+
